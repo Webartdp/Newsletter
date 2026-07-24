f902cf008e9abb6cd8b54a83d7aa5d5b31e4e020
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-07-24 15:47:15 +0300

    Initialize MODX request in connector

--- assets/components/dnepritnewsletter/connector.php
+++ assets/components/dnepritnewsletter/connector.php
@@ -6,6 +6,7 @@ require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 $modx = new modX();
 $modx->initialize('mgr');
 $modx->getService('error', 'error.modError');
+$modx->getRequest();
 
 $corePath = $modx->getOption(
     'dnepritnewsletter.core_path',
