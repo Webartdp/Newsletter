d56094db78067d9976db3f2778a7a349e713225c
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-07-24 21:12:35 +0300

    Add Ukrainian public lexicon

---
+++ core/components/dnepritnewsletter/lexicon/uk/web.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+$_lang['dnepritnewsletter_web_email_label'] = 'Email';
+$_lang['dnepritnewsletter_web_email_placeholder'] = 'Ваша email-адреса';
+$_lang['dnepritnewsletter_web_name_label'] = 'Ім’я';
+$_lang['dnepritnewsletter_web_name_placeholder'] = 'Ваше ім’я';
+$_lang['dnepritnewsletter_web_consent'] = 'Я погоджуюся отримувати інформаційні листи та можу відписатися будь-коли.';
+$_lang['dnepritnewsletter_web_button'] = 'Підписатися';
+$_lang['dnepritnewsletter_web_success'] = 'Дякуємо! Запит на підписку прийнято.';
+$_lang['dnepritnewsletter_web_err_origin'] = 'Запит надіслано з недозволеного джерела.';
+$_lang['dnepritnewsletter_web_err_token'] = 'Форма застаріла. Оновіть сторінку та спробуйте ще раз.';
+$_lang['dnepritnewsletter_web_err_too_fast'] = 'Форму надіслано надто швидко. Зачекайте кілька секунд.';
+$_lang['dnepritnewsletter_web_err_expired'] = 'Час дії форми минув. Оновіть сторінку.';
+$_lang['dnepritnewsletter_web_err_rate_limit'] = 'Забагато запитів. Спробуйте пізніше.';
+$_lang['dnepritnewsletter_web_err_email'] = 'Вкажіть коректну email-адресу.';
+$_lang['dnepritnewsletter_web_err_name'] = 'Вкажіть ім’я.';
+$_lang['dnepritnewsletter_web_err_consent'] = 'Потрібна згода на отримання розсилки.';
+$_lang['dnepritnewsletter_web_err_validation'] = 'Перевірте заповнення форми.';
+$_lang['dnepritnewsletter_web_err_save'] = 'Не вдалося обробити підписку. Спробуйте пізніше.';
+$_lang['dnepritnewsletter_web_err_session'] = 'Не вдалося створити захищену форму. Оновіть сторінку.';
+
+$_lang['dnepritnewsletter_web_unsubscribe_button'] = 'Підтвердити відписку';
+$_lang['dnepritnewsletter_web_unsubscribe_confirm'] = 'Підтвердьте, що більше не бажаєте отримувати цю розсилку.';
+$_lang['dnepritnewsletter_web_unsubscribe_success'] = 'Ви успішно відписалися від розсилки.';
+$_lang['dnepritnewsletter_web_unsubscribe_already'] = 'Ця адреса вже не отримує розсилку.';
+$_lang['dnepritnewsletter_web_unsubscribe_invalid'] = 'Посилання для відписки недійсне або застаріле.';
+$_lang['dnepritnewsletter_web_unsubscribe_expired'] = 'Форма підтвердження застаріла. Відкрийте посилання з листа ще раз.';
+$_lang['dnepritnewsletter_web_unsubscribe_save_error'] = 'Не вдалося виконати відписку. Спробуйте пізніше.';
+
+$_lang['dnepritnewsletter_web_log_public_subscribe_created'] = 'Підписника додано через публічну форму.';
+$_lang['dnepritnewsletter_web_log_public_subscribe_existing'] = 'Повторний запит активного підписника через публічну форму.';
+$_lang['dnepritnewsletter_web_log_public_subscribe_reactivated'] = 'Відписаного підписника повторно активовано через публічну форму.';
+$_lang['dnepritnewsletter_web_log_public_unsubscribe'] = 'Підписник підтвердив відписку на публічній сторінці.';
