1c952c35337a0dabb66b073692b9775f2bbb11e4
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-07-24 17:47:36 +0300

    Add Russian queue lexicon

---
+++ core/components/dnepritnewsletter/lexicon/ru/queue.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+$_lang['dnepritnewsletter_queue_prepare'] = 'Сформировать очередь';
+$_lang['dnepritnewsletter_queue_prepare_title'] = 'Подготовка рассылки';
+$_lang['dnepritnewsletter_queue_send_now'] = 'Начать отправку сразу после формирования очереди';
+$_lang['dnepritnewsletter_queue_schedule'] = 'Дата и время';
+$_lang['dnepritnewsletter_queue_scheduled_at'] = 'Запланировано';
+$_lang['dnepritnewsletter_queue_warning'] = 'В очередь попадут только активные подписчики. Для каждого будет сохранена персональная копия темы и письма. После этого кампанию нельзя будет редактировать.';
+$_lang['dnepritnewsletter_queue_prepared'] = 'Очередь сформирована: [[+count]] получателей.';
+$_lang['dnepritnewsletter_queue_log_prepared'] = 'Создано [[+count]] писем. Начало отправки: [[+date]].';
+$_lang['dnepritnewsletter_queue_err_no_recipients'] = 'Нет активных подписчиков для этой рассылки.';
+$_lang['dnepritnewsletter_queue_err_already_prepared'] = 'Эта рассылка уже подготовлена к отправке.';
+$_lang['dnepritnewsletter_queue_err_already_exists'] = 'Для этой рассылки уже существуют записи в очереди.';
+$_lang['dnepritnewsletter_queue_err_schedule_invalid'] = 'Укажите корректную дату и время отправки.';
+$_lang['dnepritnewsletter_queue_err_schedule_past'] = 'Запланированное время должно быть минимум на минуту позже текущего.';
+$_lang['dnepritnewsletter_queue_err_save'] = 'Не удалось создать письмо для [[+email]].';
+$_lang['dnepritnewsletter_queue_err_campaign_save'] = 'Не удалось обновить состояние рассылки.';
+$_lang['dnepritnewsletter_queue_err_transaction'] = 'Не удалось выполнить транзакцию формирования очереди.';
