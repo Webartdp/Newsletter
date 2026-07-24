d70e595f35241819a5545022b61054563d2629bd
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-07-24 17:08:08 +0300

    Load subscriber import scripts

--- core/components/dnepritnewsletter/controllers/mgr/home.class.php
+++ core/components/dnepritnewsletter/controllers/mgr/home.class.php
@@ -48,13 +48,12 @@ class DnepritNewsletterHomeManagerController extends modExtraManagerController
 
     public function loadCustomCssJs()
     {
-        $jsUrl = $this->dnepritNewsletter->config['jsUrl'];
-
         $this->addCss($this->dnepritNewsletter->config['cssUrl'] . 'mgr.css');
-        $this->addJavascript($jsUrl . 'mgr/widgets/subscribers.window.js');
-        $this->addJavascript($jsUrl . 'mgr/widgets/subscribers.grid.js');
-        $this->addJavascript($jsUrl . 'mgr/widgets/home.panel.js');
-        $this->addLastJavascript($jsUrl . 'mgr/sections/home.js');
+        $this->addJavascript($this->dnepritNewsletter->config['jsUrl'] . 'mgr/widgets/subscribers.window.js');
+        $this->addJavascript($this->dnepritNewsletter->config['jsUrl'] . 'mgr/widgets/subscribers.import.window.js');
+        $this->addJavascript($this->dnepritNewsletter->config['jsUrl'] . 'mgr/widgets/subscribers.grid.js');
+        $this->addJavascript($this->dnepritNewsletter->config['jsUrl'] . 'mgr/widgets/home.panel.js');
+        $this->addLastJavascript($this->dnepritNewsletter->config['jsUrl'] . 'mgr/sections/home.js');
     }
 
     public function getTemplateFile()
