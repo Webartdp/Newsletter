511d12d21b8aade130be9208939738cef1c4773d
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-07-24 15:47:05 +0300

    Load component service in home controller

--- core/components/dnepritnewsletter/controllers/mgr/home.class.php
+++ core/components/dnepritnewsletter/controllers/mgr/home.class.php
@@ -1,7 +1,41 @@
 <?php
 
-class DnepritNewsletterHomeManagerController extends modManagerController
+class DnepritNewsletterHomeManagerController extends modExtraManagerController
 {
+    /** @var DnepritNewsletter */
+    public $dnepritNewsletter;
+
+    public function initialize()
+    {
+        $corePath = $this->modx->getOption(
+            'dnepritnewsletter.core_path',
+            null,
+            $this->modx->getOption('core_path') . 'components/dnepritnewsletter/'
+        );
+
+        require_once $corePath . 'model/dnepritnewsletter/dnepritnewsletter.class.php';
+        $this->dnepritNewsletter = new DnepritNewsletter($this->modx);
+
+        $this->addJavascript($this->dnepritNewsletter->config['jsUrl'] . 'mgr/dnepritnewsletter.js');
+        $this->addHtml(
+            '<script>Ext.onReady(function(){DnepritNewsletter.config=' .
+            $this->modx->toJSON($this->dnepritNewsletter->config) .
+            ';});</script>'
+        );
+
+        return parent::initialize();
+    }
+
+    public function getLanguageTopics()
+    {
+        return ['dnepritnewsletter:default'];
+    }
+
+    public function checkPermissions()
+    {
+        return (bool)($this->modx->user->sudo || $this->modx->hasPermission('newsletter_view'));
+    }
+
     public function process(array $scriptProperties = [])
     {
         return '';
@@ -14,19 +48,14 @@ class DnepritNewsletterHomeManagerController extends modManagerController
 
     public function loadCustomCssJs()
     {
-        /** @var DnepritNewsletter $service */
-        $service = $this->modx->controller->dnepritNewsletter;
-
-        $this->addCss($service->config['cssUrl'] . 'mgr.css');
-        $this->addJavascript($service->config['jsUrl'] . 'mgr/widgets/subscribers.grid.js');
-        $this->addJavascript($service->config['jsUrl'] . 'mgr/widgets/home.panel.js');
-        $this->addLastJavascript($service->config['jsUrl'] . 'mgr/sections/home.js');
+        $this->addCss($this->dnepritNewsletter->config['cssUrl'] . 'mgr.css');
+        $this->addJavascript($this->dnepritNewsletter->config['jsUrl'] . 'mgr/widgets/subscribers.grid.js');
+        $this->addJavascript($this->dnepritNewsletter->config['jsUrl'] . 'mgr/widgets/home.panel.js');
+        $this->addLastJavascript($this->dnepritNewsletter->config['jsUrl'] . 'mgr/sections/home.js');
     }
 
     public function getTemplateFile()
     {
-        /** @var DnepritNewsletter $service */
-        $service = $this->modx->controller->dnepritNewsletter;
-        return $service->config['templatesPath'] . 'mgr/home.tpl';
+        return $this->dnepritNewsletter->config['templatesPath'] . 'mgr/home.tpl';
     }
 }
