a93c4c77c91daa2d4b054728f539e191c55bcf04
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-07-25 09:12:05 +0300

    Delete campaigns with related queue and logs

--- core/components/dnepritnewsletter/processors/campaigns/remove.class.php
+++ core/components/dnepritnewsletter/processors/campaigns/remove.class.php
@@ -1,26 +1,72 @@
 <?php
 
-class DnepritNewsletterCampaignRemoveProcessor extends modObjectRemoveProcessor
+class DnepritNewsletterCampaignRemoveProcessor extends modProcessor
 {
-    public $classKey = 'DnepritNewsletterCampaign';
-    public $languageTopics = ['dnepritnewsletter:default'];
-    public $objectType = 'dnepritnewsletter.campaign';
+    public $languageTopics = ['dnepritnewsletter:default', 'dnepritnewsletter:monitoring'];
 
-    public function beforeRemove()
+    public function process()
     {
         if (!$this->modx->user->sudo && !$this->modx->hasPermission('newsletter_campaigns_manage')) {
-            return $this->modx->lexicon('access_denied');
+            return $this->failure($this->modx->lexicon('access_denied'));
         }
 
-        if ((string)$this->object->get('status') !== 'draft') {
-            return $this->modx->lexicon('dnepritnewsletter_campaign_err_locked');
+        $id = (int)$this->getProperty('id', 0);
+        /** @var DnepritNewsletterCampaign|null $campaign */
+        $campaign = $this->modx->getObject('DnepritNewsletterCampaign', $id);
+        if (!$campaign) {
+            return $this->failure($this->modx->lexicon('dnepritnewsletter_campaign_err_not_found'));
         }
 
-        if ($this->modx->getCount('DnepritNewsletterQueue', ['campaign_id' => (int)$this->object->get('id')]) > 0) {
-            return $this->modx->lexicon('dnepritnewsletter_campaign_err_has_queue');
+        if ((string)$campaign->get('status') === 'sending') {
+            return $this->failure($this->modx->lexicon('dnepritnewsletter_campaign_err_remove_sending'));
         }
 
-        return parent::beforeRemove();
+        if ($this->modx->getCount('DnepritNewsletterQueue', [
+            'campaign_id' => $id,
+            'status' => 'processing',
+        ]) > 0) {
+            return $this->failure($this->modx->lexicon('dnepritnewsletter_campaign_err_remove_sending'));
+        }
+
+        $transactionStarted = false;
+
+        try {
+            if (!$this->modx->beginTransaction()) {
+                throw new RuntimeException($this->modx->lexicon('dnepritnewsletter_campaign_err_remove_transaction'));
+            }
+            $transactionStarted = true;
+
+            $logsTable = $this->modx->getTableName('DnepritNewsletterLog');
+            $queueTable = $this->modx->getTableName('DnepritNewsletterQueue');
+
+            $deleteLogs = $this->modx->prepare('DELETE FROM ' . $logsTable . ' WHERE `campaign_id` = ?');
+            $deleteLogs->execute([$id]);
+
+            $deleteQueue = $this->modx->prepare('DELETE FROM ' . $queueTable . ' WHERE `campaign_id` = ?');
+            $deleteQueue->execute([$id]);
+
+            if (!$campaign->remove()) {
+                throw new RuntimeException($this->modx->lexicon('dnepritnewsletter_campaign_err_remove'));
+            }
+
+            if (!$this->modx->commit()) {
+                throw new RuntimeException($this->modx->lexicon('dnepritnewsletter_campaign_err_remove_transaction'));
+            }
+            $transactionStarted = false;
+        } catch (Throwable $exception) {
+            if ($transactionStarted) {
+                $this->modx->rollBack();
+            }
+
+            $this->modx->log(
+                modX::LOG_LEVEL_ERROR,
+                '[DnepritNewsletter] Campaign removal failed: ' . $exception->getMessage()
+            );
+
+            return $this->failure($exception->getMessage());
+        }
+
+        return $this->success($this->modx->lexicon('dnepritnewsletter_campaign_removed'));
     }
 }
 
