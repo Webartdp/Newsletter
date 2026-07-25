6fd3062aa152676f4fddaae12bd0ef7defad19c7
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-07-25 08:10:52 +0300

    Load manager home controller from mgr directory

--- core/components/dnepritnewsletter/index.class.php
+++ core/components/dnepritnewsletter/index.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once MODX_CORE_PATH . 'components/dnepritnewsletter/controllers/home.class.php';
+require_once MODX_CORE_PATH . 'components/dnepritnewsletter/controllers/mgr/home.class.php';
 
 class DnepritnewsletterIndexManagerController extends DnepritNewsletterHomeManagerController
 {
